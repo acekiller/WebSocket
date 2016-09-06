0ae4cea73b4ee5f851ddb65a0a7672710940235b
Acekiller <fengxijun51020@hotmail.com>
2016-09-06 22:26:42 +0800

    Initial Commit

---
+++ WebSocket/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WebSocket
+//
+//  Created by Fantasy on 16/9/6.
+//  Copyright © 2016年 fantasy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
